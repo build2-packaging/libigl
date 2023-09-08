8d39ac9969c7196fb52e60c328ff72029e79ec56
lyrahgames <lyrahgames@mailbox.org>
2023-09-08 14:11:04 +0200

    Add basic test for static build of core module

--- libigl-core/tests/main.cpp
+++ libigl-core/tests/main.cpp
@@ -1,3 +1,26 @@
-#include <igl/all.h>
+// This simple test is taken from
+// the first tutorial of the documentation.
+//
+#include <iostream>
+//
+#include <igl/readOFF.h>
+#include <igl/writeOBJ.h>
 
-int main() {}
+using namespace std;
+
+int main(int argc, char *argv[]) {
+  Eigen::MatrixXd V;
+  Eigen::MatrixXi F;
+
+  // Load a mesh in OFF format
+  igl::readOFF("cube.off", V, F);
+
+  // Print the vertices and faces matrices
+  cout << "Vertices:\n"
+       << V << '\n'  //
+       << "Faces:\n"
+       << F << endl;
+
+  // Save the mesh in OBJ format
+  igl::writeOBJ("cube.obj", V, F);
+}
